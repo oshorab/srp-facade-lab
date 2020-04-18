4fb296b4e669abd7608916358abd01d2463e4d96
omar shorab <omarshorab@omars-MacBook-Pro.local>
2020-04-17 18:21:13 -0700

    Removed functions

--- src/srpfacadelab/RpgPlayer.java
+++ src/srpfacadelab/RpgPlayer.java
@@ -26,81 +26,15 @@ public class RpgPlayer {
         carryingCapacity = MAX_CARRYING_CAPACITY;
     }
 
-    public void useItem(Item item) {
-        if (item.getName().equals("Stink Bomb"))
-        {
-            List<IEnemy> enemies = gameEngine.getEnemiesNear(this);
-
-            for (IEnemy enemy: enemies){
-                enemy.takeDamage(100);
-            }
-        }
-    }
-
-    public boolean pickUpItem(Item item) {
-        int weight = calculateInventoryWeight();
-        if (weight + item.getWeight() > carryingCapacity)
-            return false;
-
-        if (item.isUnique() && checkIfItemExistsInInventory(item))
-            return false;
-
-        // Don't pick up items that give health, just consume them.
-        if (item.getHeal() > 0) {
-            health += item.getHeal();
-
-            if (health > maxHealth)
-                health = maxHealth;
-
-            if (item.getHeal() > 500) {
-                gameEngine.playSpecialEffect("green_swirly");
-            }
-
-            return true;
-        }
-
-        if (item.isRare())
-            gameEngine.playSpecialEffect("cool_swirly_particles");
-
-        inventory.add(item);
-
-        calculateStats();
-
-        return true;
-    }
-
-    private void calculateStats() {
-        for (Item i: inventory) {
-            armour += i.getArmour();
-        }
-    }
-
-    private boolean checkIfItemExistsInInventory(Item item) {
-        for (Item i: inventory) {
-            if (i.getId() == item.getId())
-                return true;
-        }
-        return false;
-    }
-
-    private int calculateInventoryWeight() {
-        int sum=0;
-        for (Item i: inventory) {
-            sum += i.getWeight();
-        }
-        return sum;
-    }
-
-    public void takeDamage(int damage) {
-        if (damage < armour) {
-            gameEngine.playSpecialEffect("parry");
-        }
-
-        int damageToDeal = damage - armour;
-        health -= damageToDeal;
-
-        gameEngine.playSpecialEffect("lots_of_gore");
-    }
+	 //new added helper method
+	 public void setInventory(Item item) {
+	 	  this.inventory.add(item);
+	 }
+	
+	 //new added helper method
+    public List<Item> getInventory() {
+		 return this.inventory;
+	 }
 
     public int getHealth() {
         return health;
